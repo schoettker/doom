980bc8d07d2cdc4978fe1df46bad09c6f214096c
LS8 <lennartschoettker@hotmail.com>
2020-03-16 02:21:58 +0100

    Shorten comments

--- config.el
+++ config.el
@@ -11,41 +11,30 @@
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
-;;
 ;; + `doom-font'
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'
-;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Bront" :size 18))
 (setq doom-themes-enable-bold nil)
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. These are the defaults.
-;; (setq doom-theme 'doom-solarized-light)
 (setq doom-theme 'doom-one)
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/library/Dropbox/org/")
 
-;; If you want to change the style of line numbers, change this to `relative' or
-;; `nil' to disable it:
+;; If you want to change the style of line numbers, change this to `relative' or `nil' to disable it:
 (setq display-line-numbers-type t)
 (setq display-line-numbers-type 'relative)
 (setq display-line-numbers-current-absolute nil)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
 ;;
+;; Here are some additional functions/macros that could help you configure Doom:
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', where Emacs
 ;;   looks when you load packages with `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
 ;; This will open documentation for it, including demos of how they are used.

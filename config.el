62fe350c9b80927a1447d99d8701140e215d97ce
LS8 <lennartschoettker@hotmail.com>
2020-03-16 01:56:51 +0100

    Formatting + removal of comments

--- config.el
+++ config.el
@@ -79,27 +79,34 @@
 (map! :leader
       (:n ":" #'doom/open-scratch-buffer)
       (:n "x" #'counsel-M-x)
+
       (:desc "prev buffer" :n "TAB" #'mode-line-other-buffer) ;; maybe evil-switch-to-window-last-buffer?!
       (:desc "go to char" :n "SPC" #'avy-goto-word-or-subword-1)
       (:desc "add ;" :n "RET" #'add-semicolon-at-eol)
       (:desc "swiper" :n "/" #'counsel-grep-or-swiper)
       (:desc "kill buffer" :n "d" #'kill-this-buffer)
+
       (:desc "apps" :prefix "a"
         :desc "ag" :n "a" #'ag
         :desc "dired" :n "d" #'dired
         :desc "termite" :n "t" #'open-termite)
+
       (:desc "buffer" :prefix "b"
         ;; :desc "agenda buffer" :n "a" #'(lambda () (interactive) (switch-to-buffer "*Org Agenda*"))
         :desc "counsel boomarks" :n "j" #'counsel-bookmark
         :desc "list bookmarks" :n "l" #'list-bookmarks)
+
       (:desc "e" :prefix "e"
         :desc "next error" :n "n" #'flycheck-next-error
         :desc "prev error" :n "p" #'flycheck-previous-error
         :desc "termite" :n "t" #'open-termite)
+
       (:desc "git" :prefix "g"
         :desc "status" :n "s" #'magit-status)
+
       (:desc "insert" :prefix "i"
         :desc "org table" :n "t" #'org-table-create)
+
       (:desc "Project" :prefix "p"
         :desc "search in project" :n "/" #'+ivy/project-search
         :desc "find file" :n "p" #'+ivy/projectile-find-file
@@ -118,20 +125,22 @@
         :desc "agenda headings" :n "h" #'counsel-org-agenda-headlines
         :desc "todo's" :n "t" #'org-todo-list
         :desc "open config" :n "c" #'(lambda () (interactive) (find-file "~/.doom.d/config.el")))
+
       (:desc "window" :prefix "w"
         :desc "close workspace" :n "D" #'eyebrowse-close-window-config
         :desc "maximize buffer" :n "m" #'doom/window-maximize-buffer)
+
       (:desc "z" :prefix "z"
         :desc "narrow" :n "n" #'narrow-to-region
-        :desc "widen" :n "w" #'widen)
-      )
+        :desc "widen" :n "w" #'widen))
 
 (map! :nvime "C-y" #'yank)
 (map! :nvme "\\" #'evil-ex-nohighlight)
+
 (def-package! eyebrowse
   :defer 2
   :config (eyebrowse-mode t)
-                                        ;(set-face-foreground 'eyebrowse-mode-line-active "medium turquoise")
+  ;(set-face-foreground 'eyebrowse-mode-line-active "medium turquoise")
   (set-face-foreground 'eyebrowse-mode-line-active "purple")
   (setq eyebrowse-mode-line-separator "|")
   (setq eyebrowse-new-workspace "*doom*")
@@ -141,8 +150,7 @@
    :nvie "M-3" #'eyebrowse-switch-to-window-config-3
    :nvie "M-4" #'eyebrowse-switch-to-window-config-4
    :nvie "M-5" #'eyebrowse-switch-to-window-config-5
-   )
-  )
+   ))
 
 ;; (def-package! smooth-scroll
 ;;   :config (smooth-scroll-mode 1))
@@ -159,7 +167,7 @@
         (replace-match (number-to-string (funcall change number)))
         (goto-char point)))))
 (defun my-increment-number-at-point ()
-                                        ;"Increment number at point like vim's C-a"
+  "Increment number at point like vim's C-a"
   (interactive)
   (my-change-number-at-point '1+))
 (defun my-decrement-number-at-point ()
@@ -176,28 +184,17 @@
 (setq undo-tree-auto-save-history nil)
 (setq +doom-dashboard-banner-file (expand-file-name "logo.png" doom-private-dir))
 
-
-
-;; Wrap experiment
-;; (setq default-fill-column 80)
-;; (visual-line-mode 1)
-;; (+word-wrap-mode)
-;; (setq +word-wrap--enable-visual-fill-mode t)
 (global-visual-line-mode 1)
 
 (after! magit
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-  (setq magit-save-repository-buffers t)
-  )
+  (setq magit-save-repository-buffers t))
 
 (defun create-buffer(name)
   (interactive "sEnter name for new buffer: ")
   (switch-to-buffer
    (find-file (concat "/tmp/" name))))
 
-;; (remove-hook 'org-mode-hook #'auto-fill-mode)
-;; (add-hook 'org-mode-hook (lambda () (org-indent-mode 0)))
-;; (setq org-hide-leading-stars-before-indent-mode nil)
 (add-hook 'org-mode-hook (lambda () (auto-fill-mode 0)))
 (after! org
   (add-hook 'org-agenda-mode-hook (lambda () (visual-line-mode 0)))

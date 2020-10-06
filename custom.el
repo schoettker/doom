11e393b5008fe59f2a119d9a2ec27fa0b54c0d7e
LS8 <lennartschoettker@hotmail.com>
2020-10-06 18:58:30 +0200

    add custom.el
    - maybe could be replaced by pulling out the stuff

---
+++ custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((org-export-preserve-breaks . t)
+     (org-html-htmlize-output-type . css))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ediff-current-diff-A ((t (:background "#87003f"))))
+ '(ediff-current-diff-Ancestor ((t (:background "#263854"))))
+ '(ediff-current-diff-B ((t (:background "#008758"))))
+ '(ediff-current-diff-C ((t (:background "#c4eeff"))))
+ '(ediff-even-diff-A ((t (:background "#263854"))))
+ '(ediff-even-diff-Ancestor ((t (:background "#263854"))))
+ '(ediff-even-diff-B ((t (:background "#263854"))))
+ '(ediff-even-diff-C ((t (:background "#263854"))))
+ '(ediff-fine-diff-A ((t (:background "#c9005e"))))
+ '(ediff-fine-diff-B ((t (:background "#00d188"))))
+ '(ediff-odd-diff-A ((t (:background "#263854"))))
+ '(ediff-odd-diff-Ancestor ((t (:background "#263854"))))
+ '(ediff-odd-diff-B ((t (:background "#263854"))))
+ '(ediff-odd-diff-C ((t (:background "#263854")))))

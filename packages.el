ba9fa83030e75075ec9a2f4ca14e421030b4199b
LS8 <lennartschoettker@hotmail.com>
2020-12-17 12:40:20 +0100

    Deactivate unused packages and add nano recipe

--- packages.el
+++ packages.el
@@ -52,11 +52,13 @@
 ;(package! builtin-package :recipe (:branch "develop"))
 (package! winum)
 (package! ag)
-(package! leetcode :recipe (:host github :repo "kaiwk/leetcode.el"))
 ;; (package! smooth-scroll)
 ;; (package! bufler :recipe (:host github :repo "alphapapa/bufler.el" :files (:defaults (:exclude "helm-bufler.el"))))
-(package! emms)
+;; (package! emms)
+;; (package! leetcode :recipe (:host github :repo "kaiwk/leetcode.el"))
 (package! company-ledger)
 (package! verb)
+;; (package! nano-emacs :recipe (:host github :repo "rougier/nano-emacs"))
+
 
 (disable-packages! evil-escape org-bullets org-superstar savehist saveplace org-bookmark-heading orgit org-yt)

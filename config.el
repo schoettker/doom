8252f06aea38ef115b23e75fe1952fc80617be4c
LS8 <lennartschoettker@hotmail.com>
2021-02-22 14:57:15 +0100

    theme change

--- config.el
+++ config.el
@@ -18,7 +18,9 @@
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Bront" :size 18))
 (setq doom-themes-enable-bold nil)
-(setq doom-theme 'doom-molokai)
+;; (setq doom-theme 'doom-monokai-pro)
+;; (setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-palenight)
 ;; (setq doom-theme 'doom-horizon) ; 2nd favorite and good for diffing!
 ;; (setq doom-theme 'doom-one) ; probably the best diffing
 

2ec23d3468c2e6a14f879cc3e299c3fbfe6cf30e
Lennart Schoettker <lennartschoettker@hotmail.com>
2022-02-04 01:29:11 +0100

    add & configure dired sidebar

--- config.el
+++ config.el
@@ -256,3 +256,22 @@
         tex-mode         ; latexindent is broken
         c++-mode
         latex-mode))
+(use-package! dired-sidebar
+  ;; :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+
+  ;; (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  ;; (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'icons)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))

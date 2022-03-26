85b7ec1ede8acc17670cf45c4e03007386a20000
Lennart Schoettker <lennartschoettker@hotmail.com>
2022-03-26 18:27:08 +0100

    activate org log into drawer to track state changes of (recurring) todo items etc

--- +org.el
+++ +org.el
@@ -11,6 +11,7 @@
 
   ;; (setq org-ellipsis " ▼ ") ;; folding symbol
   ;; (setq org-ellipsis "  ")  ;; folding symbol
+  (setq org-log-into-drawer t)
   (setq org-ellipsis " ▼ ")  ;; folding symbol
   (setq org-cycle-separator-lines -1) ;; to not consider blank lines as part of heading and resolve display issue with ellipsis char and []
   (setq org-export-with-clocks t)

8255ae925c979f5e6fcd36462e6d8e9f3d2f8502
Lennart Schöttker <lennartschoettker@hotmail.com>
2024-07-24 18:51:19 +0200

    setup obsidian pkg

--- config.el
+++ config.el
@@ -107,3 +107,31 @@
   ;; https://github.com/emacs-lsp/lsp-mode/issues/3577#issuecomment-1709232622
   (delete 'lsp-terraform lsp-client-packages))
 (setenv "PATH" (concat (getenv "PATH") ":/Users/lschoettker/dev/go/bin"))
+
+
+
+;; (use-package obsidian
+;;   :demand t
+;;   :config
+;;   (obsidian-specify-path "~/dizzy")
+;;   (global-obsidian-mode t)
+;;   :custom
+;;   ;; This directory will be used for `obsidian-capture' if set.
+;;   (obsidian-inbox-directory "Inbox")
+;;   ;; Create missing files in inbox? - when clicking on a wiki link
+;;   ;; t: in inbox, nil: next to the file with the link
+;;   ;; default: t
+;;                                         ;(obsidian-wiki-link-create-file-in-inbox nil)
+;;   ;; The directory for daily notes (file name is YYYY-MM-DD.md)
+;;   (obsidian-daily-notes-directory "Daily Notes")
+;;   ;; Directory of note templates, unset (nil) by default
+;;                                         ;(obsidian-templates-directory "Templates")
+;;   ;; Daily Note template name - requires a template directory. Default: Daily Note Template.md
+;;                                         ;(setq obsidian-daily-note-template "Daily Note Template.md")
+;;   :bind (:map obsidian-mode-map
+;;               ;; Replace C-c C-o with Obsidian.el's implementation. It's ok to use another key binding.
+;;               ("C-c C-o" . obsidian-follow-link-at-point)
+;;               ;; Jump to backlinks
+;;               ("C-c C-b" . obsidian-backlink-jump)
+;;               ;; If you prefer you can use `obsidian-insert-link'
+;;               ("C-c C-l" . obsidian-insert-wikilink)))

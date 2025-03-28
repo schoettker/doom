c3449a72f0b9244015e6b9924c11e9beca752590
Lennart Schöttker <lennartschoettker@hotmail.com>
2025-03-28 19:21:52 +0100

    theme changes

--- config.el
+++ config.el
@@ -36,9 +36,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(if (not (display-graphic-p))
-    (setq doom-theme 'doom-monokai-octagon)
-  (setq doom-theme 'doom-snazzy))
+;; (if (not (display-graphic-p))
+;;     (setq doom-theme 'doom-monokai-octagon))
+;;   (setq doom-theme 'doom-snazzy)
+(setq doom-theme 'doom-monokai-octagon)
+;; (load-theme 'doom-monokai-octagon)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.

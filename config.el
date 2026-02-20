b50e8216d6cc7913a9d1c77b27914a4248972bc4
Lennart Schöttker <lennartschoettker@hotmail.com>
2026-02-20 10:32:36 +0100

    AI Cleanup: Line numbers

--- config.el
+++ config.el
@@ -24,11 +24,7 @@
 ;; (setq doom-theme 'doom-tomorrow-day) ;; light theme
 (setq doom-theme 'doom-xcode)
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
 (setq display-line-numbers-type 'relative)
-;; (setq display-line-numbers-current-absolute nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!

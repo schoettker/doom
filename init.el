10727d5d48a8916006ddec981811fab84705262c
Lennart Schöttker <lennartschoettker@hotmail.com>
2026-02-22 12:58:11 +0100

    Switch from company to corfu + orderless

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (corfu +orderless) ; fast, native completion with fuzzy matching
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life

df503513f6f66e127d786c17883d8e14ff1fa07f
Lennart Schoettker <lennartschoettker@hotmail.com>
2026-07-18 22:46:45 +0200

    ghostel and zen pkg

--- init.el
+++ init.el
@@ -55,7 +55,7 @@
        ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen                 ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -82,7 +82,8 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       ;; vterm             ; the best terminal emulation in Emacs
+       ghostel
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
